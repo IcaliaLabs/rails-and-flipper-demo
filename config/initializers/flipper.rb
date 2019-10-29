cf2470ea7a84a5ee7e1ba382afc4aeb591d18a09
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-10-29 17:50:38 -0600

    Fixed flipper redis configuration

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,10 +1,18 @@
 require 'flipper'
+require 'flipper/adapters/redis'
 
 Flipper.configure do |config|
   config.default do
+    adapter_arguments = []
     adapter_class_name = Rails.env.test? ? 'Memory' : 'Redis'
+
+    if adapter_class_name == 'Redis'
+      redis_client = Redis.new
+      adapter_arguments << redis_client
+    end
+
     adapter_class = "Flipper::Adapters::#{adapter_class_name}".safe_constantize
-    adapter = adapter_class.new
+    adapter = adapter_class.new *adapter_arguments
 
     # pass adapter to handy DSL instance
     Flipper.new(adapter)
