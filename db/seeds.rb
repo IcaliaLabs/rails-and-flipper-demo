398cb36248a27aa1895890d1ef09cfc340aa876a
Roberto Quintanilla <1915931+vovimayhem@users.noreply.github.com>
2019-10-30 19:16:00 -0600

    Add feature toggle specs (#6)
    * Added a spec for the new feature
    
    * Added release feature toggles to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Release Feature Toggles - Keep only the ones currently on development.
+Flipper.add :new_people_form
