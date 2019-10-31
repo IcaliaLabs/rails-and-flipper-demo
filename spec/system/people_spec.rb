ead40763a77a3662158d2052280cbebeb56cb1bc
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-10-30 18:50:31 -0600

    Added a spec for the new feature

--- spec/system/people_spec.rb
+++ spec/system/people_spec.rb
@@ -8,10 +8,26 @@ RSpec.describe 'People', type: :system do
   describe 'Create a person entry' do
     scenario 'should be succesful' do
       visit new_person_path
+      expect(page).to have_content 'Old Form..'
       fill_in 'First name', with: 'Subject'
       fill_in 'Last name', with: 'Test'
       fill_in 'Email', with: 'test@example.com'
       expect { click_button 'Create Person' }.to change(Person, :count).by 1
+      expect(page).to have_content 'Person was successfully created'
+    end
+
+    context 'with the new person form' do
+      before { Flipper[:new_people_form].enable }
+
+      scenario 'should be succesful' do
+        visit new_person_path
+        expect(page).to have_content 'New Form...'
+        fill_in 'First name', with: 'Subject'
+        fill_in 'Last name', with: 'Test'
+        fill_in 'Email', with: 'test@example.com'
+        expect { click_button 'Create Person' }.to change(Person, :count).by 1
+        expect(page).to have_content 'Person was successfully created'
+      end
     end
   end
 end
